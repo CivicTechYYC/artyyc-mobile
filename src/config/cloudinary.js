5db5024571fc35d048bddb4726b11de9db2e7e64
Scott Veale <scottveale@Scotts-MacBook-Pro.local>
2018-03-03 15:48:51 -0700

    cloudinary upload with image picker

---
+++ src/config/cloudinary.js
@@ -0,0 +1,8 @@
+const config = {
+	cloud_name: "dbeg0e56f",
+	api_key: "446827124576434",
+	api_secret: "uSoQ_fU-E4VyXW80W14x1Lwr6go",
+	environment: "CLOUDINARY_URL=cloudinary://446827124576434:uSoQ_fU-E4VyXW80W14x1Lwr6go@dbeg0e56f"
+}
+
+export default config;
