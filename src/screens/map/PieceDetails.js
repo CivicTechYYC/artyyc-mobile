625d14f9494d03725fc479c30c924277391cfcf7
Lee Mulvey <lmulvey@me.com>
2018-03-06 17:30:27 -0700

    Further styling of the PieceDetails screen. Conditionally concatenated longDesc* to fullDesc.

--- src/screens/map/PieceDetails.js
+++ src/screens/map/PieceDetails.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { H3 } from 'native-base';
 import Icon from "react-native-vector-icons/Ionicons";
 import { Image } from "react-native";
 import MapStyles from './styles';
+const placeholderImage = require('../../assets/images/placeholderImage.png');
+
 import {
   Container,
   Header,
@@ -16,27 +19,42 @@ import {
 } from "native-base";
 
 class PieceDetails extends Component {
+
   render() {
     let { details } = this.props.navigation.state.params;
+    let fullDesc = details.longDesc1;
+    fullDesc += (details.longDesc2) ? ` ${details.longDesc2}` : '';
+    fullDesc += (details.longDesc3) ? ` ${details.longDesc3}` : '';
+    fullDesc += (details.longDesc4) ? ` ${details.longDesc4}` : '';
+
     return (
       <Container>
         <Content>
           <Card>
             <CardItem>
-              <Body>
-                <Text style={MapStyles.PieceDetails.titleText}>{details.title}</Text>
-                <Text>A piece by {details.artist}</Text>
+              <Body style={{ marginTop: 25, marginBottom: 25 }}>
+                <Text adjustFontToFit style={MapStyles.PieceDetails.titleText}>{details.title}</Text>
+                <H3 style={[MapStyles.PieceDetails.pieceContent, {fontStyle: 'italic'}]}>A piece by {details.artist}</H3>
               </Body>
             </CardItem>
             <CardItem cardBody>
               <Image
-                source={{ uri: "Image URL" }}
-                style={{ height: 200, width: null, flex: 1 }}
+                source={placeholderImage}
+                style={MapStyles.PieceDetails.pieceImage}
               />
+              
             </CardItem>
             <CardItem>
               <Body>
-                <Text>{details.longDesc1}</Text>
+              <Text 
+                style={[MapStyles.PieceDetails.pieceContent, 
+                  MapStyles.PieceDetails.pieceAddress]}>
+                <Icon name="ios-pin" 
+                  style={[MapStyles.PieceDetails.pieceContent, 
+                  MapStyles.PieceDetails.pieceAddress, { color: 'maroon', fontSize: 18 }]}/>
+                  <Text>   </Text>{details.address}
+              </Text>
+                <Text style={MapStyles.PieceDetails.pieceContent}>{fullDesc}</Text>
               </Body>
             </CardItem>
           </Card>
